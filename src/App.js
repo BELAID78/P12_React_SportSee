402ddaffbe35d820e50d01586bfc6d183d71a6db
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-05 18:48:23 +0100

    Integrate the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
     <>
+      {/* router */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" end element={<Profile />}/>
+        </Routes>
+      </BrowserRouter>
 
-    </>     
+      {/* navbar */}
+
+      {/* side bar */}
+
+      {/* outlet */}
+    </>
   );
 }
-
 export default App;
