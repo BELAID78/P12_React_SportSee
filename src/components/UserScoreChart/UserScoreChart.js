b88625e6cea71f3079eb1dfc9ea05fdd5c77566f
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-06 01:42:29 +0100

    integrate the UserScoreChart component

---
+++ src/components/UserScoreChart/UserScoreChart.js
@@ -0,0 +1,26 @@
+import { PieChart, Pie, ResponsiveContainer, Legend } from "recharts"
+
+const renderLegend = (score) => {
+    return (
+      <div className="user-chart-score-legend">
+        <p>{score[0].name}</p>
+        <p>de votre objectif</p>
+      </div>
+    );
+}
+
+function UserScoreChart({score}) {
+    return (
+        <div className="user-chart-score">
+            <p className="chart-legend">Score</p>
+            {renderLegend(score)}
+            <ResponsiveContainer height={270}>
+                <PieChart>
+                    <Pie data={score} cornerRadius={10} innerRadius={90} outerRadius={100} paddingAngle={0} dataKey="x" />
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default UserScoreChart
