752733a7b2a72517c7957147095262747fc731dd
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-08 19:09:27 +0100

    Fix the route to handle user Id

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import UserActivityChart from "../components/UserActivityChart/UserActivityChart";
 import UserAverageSession from "../components/UserAverageSession/UserAverageSession";
 import UserCardInformations from "../components/UserCardInformations/UserCardInformations";
@@ -7,6 +8,8 @@ import UserScoreChart from "../components/UserScoreChart/UserScoreChart";
 function Profile() {
     const userName = 'Thomas';
 
+    const userId = useParams().id;
+    
     const userActivity = [
         {
             day: '2020-07-01',
