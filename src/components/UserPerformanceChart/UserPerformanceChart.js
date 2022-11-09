0a2dba9b62f027bf26ad207de06c92c014de5dbd
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-09 18:03:00 +0100

    Integrate the spinner for every component

--- src/components/UserPerformanceChart/UserPerformanceChart.js
+++ src/components/UserPerformanceChart/UserPerformanceChart.js
@@ -1,9 +1,14 @@
 import { useContext } from "react"
 import { RadarChart, PolarGrid, PolarAngleAxis, Radar, ResponsiveContainer } from "recharts"
 import { UserPerformanceContext } from "../../pages/Profile"
+import Spinner from "../Spinner/Spinner";
 
 function UserPerformanceChart() {
     const userPerformance = useContext(UserPerformanceContext);
+
+    if(userPerformance.length === 0) {
+        return <Spinner />
+    }
 
     return (
         <div className="user-chart-performance">
