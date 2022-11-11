9a0fff13b8178adf8fe5b3779f9eaf7c1fde3d33
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-11 16:43:43 +0100

    integrate NavBarItem component

---
+++ src/components/NavBarItem/NavBarItem.js
@@ -0,0 +1,38 @@
+import PropType from 'prop-types';
+
+/**
+ * Component for render navbar item
+ * 
+ * @param {object} 
+ * @returns {React.ReactElement}
+ */
+function NavBarItem({children, link = '#', logo = null , classname = '', alt = ''}) {
+    return (
+        <li>
+            <a href={link}>
+                { logo && <img className={classname} src={logo} alt={alt} /> }
+                {children}
+            </a>
+        </li>
+    )
+}
+
+//set NavBarItem propTypes
+NavBarItem.propTypes = {
+    children: PropType.string,
+    link: PropType.string,
+    logo: PropType.string,
+    classname: PropType.string,
+    alt: PropType.string
+}
+
+//set NavBarItem defaultProps
+NavBarItem.defaultProps = {
+    link: '#',
+    logo: null,
+    classname: '',
+    alt: ''
+}
+
+//export NavBarItem component
+export default NavBarItem
